fc2f1fbc2128a475f106b896fa9f77459ce1e203
Thiago Matheus <105510456+thimatheus@users.noreply.github.com>
2025-07-30 18:06:30 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, Query
+import pandas as pd
+
+app = FastAPI()
+
+# Carrega os dados uma vez ao iniciar a API
+df = pd.read_excel("baseficticiarubbycx.xlsx")
+
+@app.get("/dados")
+def consultar_dados(cpf: str = Query(..., description="Digite o CPF sem pontos ou traços")):
+    resultado = df[df['CPF'].astype(str) == cpf]
+    
+    if resultado.empty:
+        return {"erro": "CPF não encontrado"}
+    
+    dados = resultado[[
+        'Consumo (kWh)',
+        'Geração (kWh)',
+        'Crédito aplicado (kWh)',
+        'Fatura R$',
+        'Saldo de crédito'
+    ]].to_dict(orient="records")
+    
+    return {"cpf": cpf, "dados": dados}
+
